4dd37ac3cd6c9a27f573c63a94fbe9c089668f05
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2025-02-19 22:08:25 +0600

    express routing

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// Initialize Express app
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+const userPost = require("./src/routes/userPost.route");
+app.use("/userPost", userPost);
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`🚀 Server is running on port: ${port}`);
 });
